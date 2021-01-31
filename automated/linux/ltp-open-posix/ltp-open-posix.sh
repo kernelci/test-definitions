8c8d09dda67b210036a0705e7c44cf8e61d9320f
lakshmipathi.g <lakshmipathi.ganapathi@collabora.com>
2021-01-31 23:25:52 +0530

    LTP: Update ltp-open-posix to run specific group test.
    Run specific group test from ltp-open-posix. Parse the
    results and send it to lava-test-case.
    
    Signed-off-by: lakshmipathi ganapathi <lakshmipathi.ganapathi@collabora.com>

--- automated/linux/ltp-open-posix/ltp-open-posix.sh
+++ automated/linux/ltp-open-posix/ltp-open-posix.sh
@@ -6,6 +6,8 @@ set -x
 . ../../lib/sh-test-lib
 OUTPUT="$(pwd)/output"
 RESULT_FILE="${OUTPUT}/result.txt"
+GRP_RESULT_FILE="${OUTPUT}/grp_result.txt"
+GRP_OUTPUT_FILE="${OUTPUT}/grp_output.txt"
 # Absolute path to this script. /home/user/bin/foo.sh
 SCRIPT="$(readlink -f "${0}")"
 # Absolute path this script is in. /home/user/bin
@@ -20,10 +22,11 @@ LTP_VERSION="20180515"
 LTP_PRE_INSTALL=/opt/ltp
 LTP_PATH=/opt/ltp/testcases/open_posix_testsuite
 
-while getopts "s:v:" arg; do
+while getopts "s:v:t:" arg; do
    case "$arg" in
      s) SKIP_INSTALL="${OPTARG}";;
      v) LTP_VERSION="${OPTARG}";;
+     t) GRP_TEST="${OPTARG}";;
      *) ;;
    esac
 done
@@ -63,6 +66,16 @@ parse_ltp_output() {
 
 }
 
+# Parse LTP open posix grp output
+parse_ltp_grp_output() {
+    sed -i '/^PASS\|^FAIL\|^Testing/!d' ${GRP_OUTPUT_FILE}
+    paste -d " "  - - - < ${GRP_OUTPUT_FILE} | tee ${GRP_RESULT_FILE}
+    sed -i 's/\s\s*/ /g' ${GRP_RESULT_FILE}
+    sed -i 's/Testing //g' ${GRP_RESULT_FILE}
+    grep -E "FAIL 0" ${GRP_RESULT_FILE} | awk '{print $1 " " $2}' | sed 's/PASS/pass/; s/FAIL/fail/; s/CONF/skip/' >> ${RESULT_FILE}
+    grep -v "FAIL 0" ${GRP_RESULT_FILE} | awk '{print $1 " " $4}' | sed 's/PASS/pass/; s/FAIL/fail/; s/CONF/skip/'  >> ${RESULT_FILE}
+}
+
 # Run LTP test suite
 run_ltp_open_posix() {
     # shellcheck disable=SC2164
@@ -72,7 +85,17 @@ run_ltp_open_posix() {
       make "${EACH_TEST}"-test || true
     done
     parse_ltp_output
+
 }
+
+# Run specific LTP open posix group tests
+run_ltp_open_posix_grp() {
+    # shellcheck disable=SC2164A
+    GRP_TEST=$1
+    /opt/openposix_testsuite/bin/run-posix-option-group-test.sh ${GRP_TEST} | tee "${GRP_OUTPUT_FILE}"
+    parse_ltp_grp_output
+}
+
 
 # Test run.
 ! check_root && error_msg "This script must be run as root"
@@ -103,4 +126,9 @@ else
     install_ltp_open_posix
 fi
 info_msg "Running run-ltp-open-posix"
-run_ltp_open_posix
+
+if [ "${GRP_TEST}" != "" ]; then
+    run_ltp_open_posix_grp ${GRP_TEST}
+else
+    run_ltp_open_posix
+fi
