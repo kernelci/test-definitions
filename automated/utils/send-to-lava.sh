a222a027c55ee8d2dc476f98a337dfa41f01e5ab
André Almeida <andrealmeid@collabora.com>
2021-06-02 15:44:27 -0300

    send-to-lava: Use the last item for the result
    Some tests may have whitespace in the test name, so the "result" value
    will be part of the test description, instead of the proper result. Use
    the last item on that string to get the result in a more reliable way.
    
    Signed-off-by: André Almeida <andrealmeid@collabora.com>

--- automated/utils/send-to-lava.sh
+++ automated/utils/send-to-lava.sh
@@ -11,7 +11,7 @@ if [ -f "${RESULT_FILE}" ]; then
     while read -r line; do
         if echo "${line}" | grep -iq -E ".* +(pass|fail|skip|unknown)$"; then
             test="$(echo "${line}" | awk '{print $1}')"
-            result="$(echo "${line}" | awk '{print $2}')"
+            result="$(echo "${line}" | awk '{print $NF}')"
 
             if [ "${lava_test_case}" -eq 0 ]; then
                 lava-test-case "${test}" --result "${result}"
